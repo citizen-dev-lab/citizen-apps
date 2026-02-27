726b3a626457a0af8c8bf542cd9de1081dfe8fce
dev-r-okamoto-cyber <dev.r.okamoto.cyber+2@gmail.com>
2026-02-27 17:49:33 +0900

    Create app.py

---
+++ apps/hello/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "hello citizen-dev! ✅\n"
+
+if __name__ == "__main__":
+    # Cloud RunはPORT環境変数で指定してくる
+    import os
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host="0.0.0.0", port=port)
